2ba6d76284628a18bee398cfa2cb251e39aebbdf
shicks1080 <stuart.hicks@gmail.com>
2018-08-29 11:09:59 +0200

    Added Firebase info

--- src/App.js
+++ src/App.js
@@ -2,9 +2,31 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from '@firebase/app';
+import '@firebase/auth';
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID
+} from 'react-native-dotenv';
 import reducers from './reducers';
 
+
 class App extends Component {
+  componentWillMount() {
+    const firebaseConfig = {
+      apiKey: API_KEY,
+      authDomain: AUTH_DOMAIN,
+      databaseURL: DATABASE_URL,
+      projectID: PROJECT_ID,
+      storageBucket: STORAGE_BUCKET,
+      messagingSenderID: MESSAGING_SENDER_ID
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
   render() {
     return (
       <Provider store={createStore(reducers)}>
